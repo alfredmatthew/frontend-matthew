c72a18ced6a357ff74a8c426834e69a55937a0ad
alfredmatthew <51317708+alfredmatthew@users.noreply.github.com>
2024-05-10 12:45:30 +0700

    done all coding 1

--- src/api/apiForgotPassword.jsx
+++ src/api/apiForgotPassword.jsx
@@ -10,8 +10,17 @@ import useAxios from ".";
     }
   };
 
+  export const findUserBasedFromToken = async (token) => {
+    try {
+      const response = await useAxios.post("/find-user-token", { token }); // Send token as part of the request body
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  };  
+  
   // Mengedit Bahan Baku
-  export const ChangePassword = async (values, token) => {
+  export const ChangePasswordToken = async (values, token) => {
     try {
       const response = await useAxios.post(`/change-password/${token}`, values, {
         headers: {
--- src/pages/auth/ChangePassword.jsx
+++ src/pages/auth/ChangePassword.jsx
@@ -1,134 +1,5 @@
-// import React, { useState } from "react";
-// import { changePassword } from "../../api/user";
-// import { useNavigate } from "react-router-dom";
-// import Swal from "sweetalert2";
-// import ReactLoading from "react-loading";
-// import { Card, CardHeader, CardBody, Typography, Input, Button } from "@material-tailwind/react";
-
-// const ChangePassword = () => {
-//     const [oldPassword, setOldPassword] = useState("");
-//     const [newPassword, setNewPassword] = useState("");
-//     const [newPassword2, setNewPassword2] = useState("");
-//     const [loading, setLoading] = useState(false);
-//     const navigate = useNavigate();
-        
-    
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-//         setLoading(true);
-
-//         if (oldPassword == "" || newPassword == "" || newPassword2 == "") {
-//             Swal.fire({
-//             title: "Failed",
-//             text: "All fields are required",
-//             icon: "error",
-//             });
-//             setLoading(false);
-//             return;
-//         }
-
-//         if (newPassword !== newPassword2) {
-//             Swal.fire({
-//             title: "Failed",
-//             text: "New password does not match",
-//             icon: "error",
-//             });
-//             setLoading(false);
-//             return;
-//         }
-    
-//         changePassword({ oldPassword, newPassword })
-//         .then((res) => {
-//             Swal.fire({
-//             title: "Success",
-//             text: res.message,
-//             icon: "success",
-//             });
-//             navigate("/dashboard");
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//             Swal.fire({
-//             title: "Failed",
-//             text: err.message,
-//             icon: "error",
-//             });
-//             setLoading(false);
-//         });
-//     };
-    
-//     return (
-//         <Card className="h-full w-full">
-//             <CardHeader floated={false} shadow={false} className="rounded-none">
-//                 <div className="mb-0 flex items-center justify-between gap-8">
-//                     <div>
-//                     <Typography variant="h5" color="blue-gray">
-//                         Change Password
-//                     </Typography>
-//                     </div>
-//                 </div>
-//             </CardHeader>
-//             <CardBody>
-//                 <form onSubmit={handleSubmit}>
-//                     <div className="mb-4 flex w-full flex-col">
-//                         <Input 
-//                             label="Old Password"
-//                             type="password"
-//                             className=""
-//                             value={oldPassword}
-//                             onChange={(e) => setOldPassword(e.target.value)}
-//                             color="orange"
-//                             variant="outlined"
-//                             size="lg"
-//                         />
-//                     </div>
-//                     <div className="mb-4 flex w-full flex-col">
-//                         <Input 
-//                             label="New Password"
-//                             type="password"
-//                             className=""
-//                             value={newPassword}
-//                             onChange={(e) => setNewPassword(e.target.value)}
-//                             color="orange"
-//                             variant="outlined"
-//                             size="lg"
-//                         />
-//                     </div>
-//                     <div className="mb-4 flex w-full flex-col">
-//                         <Input
-//                             label="Confirm New Password"
-//                             type="password"
-//                             className=""
-//                             value={newPassword2}
-//                             onChange={(e) => setNewPassword2(e.target.value)}
-//                             color="orange"
-//                             variant="outlined"
-//                             size="lg"
-//                         />
-//                     </div>
-//                     <Button
-//                         variant="filled"
-//                         color="orange"
-//                         size="md"
-//                         type="submit"
-//                     >
-//                         {loading ? (
-//                         <ReactLoading type="spin" color="#ffffff" height={30} width={30} />
-//                         ) : (
-//                         "Change Password"
-//                         )}
-//                     </Button>
-//                 </form>
-//             </CardBody>
-//         </Card>
-        
-//     );
-// }
-
-// export default ChangePassword;
-
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom'; // Import useParams
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import {
   Card,
   CardHeader,
@@ -138,22 +9,40 @@ import {
   Input,
   Button,
 } from "@material-tailwind/react";
+import { findUserBasedFromToken, ChangePasswordToken } from '../../api/apiForgotPassword';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function ChangePassword() {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [isPasswordValid, setIsPasswordValid] = useState(true);
-  
-  // Use useParams to get the token from the URL
+  const [userEmail, setUserEmail] = useState('');
   const { token } = useParams();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await findUserBasedFromToken(token);
+        if (response.data) {
+          setUserEmail(response.data.email);
+        }
+      } catch (error) {
+        setUserEmail("-");
+        console.error('Error fetching user:', error);
+      }
+    };
+
+    fetchUser();
+  }, [token]);
 
   const validatePassword = (password) => {
     const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     return regex.test(password);
   };
 
-  const handleResetPassword = () => {
+  const handleResetPassword = async () => {
     if (newPassword !== confirmPassword) {
       setPasswordError('Passwords do not match.');
       setIsPasswordValid(false);
@@ -166,7 +55,14 @@ export function ChangePassword() {
       return;
     }
 
-    console.log('Password reset successfully');
+    try {
+      await ChangePasswordToken({ newPassword }, token);
+      toast.success('Password reset successfully');
+
+    } catch (error) {
+      console.error('Error resetting password:', error);
+      toast.error('Failed to reset password');
+    }
   };
 
   const handleChangeNewPassword = (e) => {
@@ -181,23 +77,21 @@ export function ChangePassword() {
     setIsPasswordValid(true);
   };
 
-
   return (
     <div className="flex justify-center items-center mt-12">
       <Card className="w-96">
         <CardHeader
           variant="gradient"
           color="gray"
-          className="mb-4 grid h-28 place-items-center"
+          className="grid h-28 place-items-center"
         >
           <Typography variant="h3" color="white">
             Reset Password
           </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4">
-          {/* Display the token in the Typography component */}
           <Typography variant="body" className="text-sm text-gray-500">
-            Your token: {token}
+            Reset password for {userEmail}
           </Typography>
           <Input
             label="New Password"
@@ -226,7 +120,7 @@ export function ChangePassword() {
         </CardBody>
         <CardFooter className="pt-0">
           <Button variant="gradient" fullWidth onClick={handleResetPassword}>
-            Recover Account
+            Reset Password
           </Button>
         </CardFooter>
       </Card>
